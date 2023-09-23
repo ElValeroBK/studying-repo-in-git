ab664c44bd56cb7e0641c76f961b25faafca1843
ElValeroBK <info.elvalerophonerepair@gmail.com>
2023-09-23 14:42:40 -0500

    acutualisando en casa09/23/2023

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -4,10 +4,15 @@ app  = FastAPI()
 
 @app.get("/")
 
-async def main():
-    return {"url": "Hola users!!"}
+async def root():
+    return {"Hello users!!"}
 
-
+@app.get("/url")
+async def url():
+    return {"url":"https://ElValeroBK.com/python/yasiel"}
 
 
 ## uvicorn main:app --reload
+## stop server (ctrl+C)
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
--- backend/type_hints.py
+++ backend/type_hints.py
@@ -7,3 +7,28 @@ my_string2 = "this is my str with hints2"
 items: list[str] = ["lolo","lala"]
 
 print(type(items))
+
+### Type Hints ###
+
+my_string_variable = "My String variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_variable: str = "My typed String variable"
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+my_typed_variable = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+print(my_string.title())
+
+def say_hi(name: str | None):
+    print(f"Hey {name}!")
+
+say_hi("")
